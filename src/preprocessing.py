4216f9b36a79f73255960b926ec95b29ca18273c
Reik Selent <r.selent@web.de>
2021-03-13 22:03:33 +0100

    update preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,3 +5,9 @@ df = pd.read_csv(r"C:\Users\rsele\OneDrive\Data Science\Projekt\ML_with_SQL_Tabl
 
 
 print(df)
+
+import numpy as np
+
+print(np.linspace(1,2,10))
+import matplotlib.pyplot as plt
+import sklearn
