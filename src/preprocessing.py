22367d043788dcc1e0916f448b3c68384de0086b
Reik Selent <r.selent@web.de>
2021-03-14 20:34:56 +0100

    move model_comparison to src

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -57,9 +57,11 @@ from col_transformer import ColTransformer
 col_trans = ColTransformer(X_train,X_test)
 scaled_X_train, scaled_X_test = col_trans.col_transformer()
 
-from model.model_comparison import ModelComp
+from model_comparison import ModelComp
 mc = ModelComp()
 
-comp = mc.models("lr","rf")
+comp = mc.models(lr="lr",rf="rf")
 
-dfs = mc.comparison()
+dfs = mc.comparison(scaled_X_train, scaled_X_test, y_train,y_test)
+
+print(dfs)
