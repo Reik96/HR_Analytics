a5cd25e9a0ace2179d1a12c93ef28fa3735bbcc8
Reik Selent <r.selent@web.de>
2021-03-15 21:09:52 +0100

    init validation

---
+++ model/validation.py
@@ -0,0 +1,10 @@
+
+from src.preprocessing import scaled_X_train,scaled_X_test, y_train,y_test
+from model_comparison import ModelComp
+mc = ModelComp()
+
+comp = mc.models(lr="lr",knn="knn",rf="rf",nb="nb",svm="svm")
+
+dfs = mc.comparison(scaled_X_train, scaled_X_test, y_train,y_test)
+
+print(dfs)
