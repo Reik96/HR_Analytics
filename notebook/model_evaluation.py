6daee0d3bd831662481ac9353027ee8f2c274fad
Reik Selent <r.selent@web.de>
2021-03-19 16:35:11 +0100

    fixed error

--- notebook/model_evaluation.py
+++ notebook/model_evaluation.py
@@ -9,23 +9,6 @@ from sklearn.metrics import classification_report
 lr = LogisticRegression(random_state=seed, max_iter=5000)
 lr.fit(scaled_X_train,y_train)
 
-y_pred = svm.predict(scaled_X_test)
+y_pred = lr.predict(scaled_X_test)
 
-print(classification_report(y_test,y_pred))
-
-
-
-def mysql(user, password, host, db):
-
-    from sqlalchemy import create_engine
-    import pandas as pd
-    engine = create_engine("mysql+pymysql://{0}:{1}@{2}/{3}".format(user, password, host, db))
-    connection = engine.connect()
-    df = pd.to_sql(select, engine)
-    return df
-
-#from src.predictions.sql_connection import mysql
-
-predictions = mysql("root",123456,"localhost","mydb")
-
-predictions.to_sql(name="mydb")
+#print(classification_report(y_test,y_pred))
