4aa72c3bc1ff4df3aee6f244991f8290d739d840
Reik Selent <r.selent@web.de>
2021-03-19 16:03:55 +0100

    simple query feature

--- src/predictions/sql_connection.py
+++ src/predictions/sql_connection.py
@@ -1,11 +1,41 @@
 class SQL:
-
-    def __init__(self):
     
+    def __init__(self, host,user,pw,db,table):
+        self.host = host
+        self.user = user
+        self.pw = pw
+        self.db = db
+        self.table = table
+
     def connect(self):
         # connect with the desired DB
-    def save_data(self):
-
+        import mysql.connector
+        from mysql.connector import MySQLConnection,Error
+        import pandas as pd
+        try:
+            conn = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=str(self.pw),
+                db = self.db,
+            )
+            cursor = conn.cursor(buffered=True)
+            #data = cursor.execute("SELECT * FROM aug_train")
+            query = "SELECT * FROM " + self.table
+            df = pd.read_sql(query,con=conn)
+            return df
+        except Error as e:
+            print(e)
+    
+    #def save_data(self):
         # Read data into DB
+
+        
     def query_data(self):
-        #Queryy data from db
+        #Query data from db
+        self.connect.execute("SELECT * FROM "+ self.table)
+
+
+c = SQL("localhost","root",123456,"hr_analytics","aug_train")
+c= c.connect()
+print(c)
