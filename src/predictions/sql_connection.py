f9d0a8c42067a57fea6edb5ee865a7de40fd6ee0
Reik Selent <r.selent@web.de>
2021-03-19 16:22:40 +0100

    init insert_data

--- src/predictions/sql_connection.py
+++ src/predictions/sql_connection.py
@@ -7,7 +7,7 @@ class SQL:
         self.db = db
         self.table = table
 
-    def connect(self):
+    def query_data(self):
         # connect with the desired DB
         import mysql.connector
         from mysql.connector import MySQLConnection,Error
@@ -20,22 +20,36 @@ class SQL:
                 db = self.db,
             )
             cursor = conn.cursor(buffered=True)
-            #data = cursor.execute("SELECT * FROM aug_train")
             query = "SELECT * FROM " + self.table
             df = pd.read_sql(query,con=conn)
+            conn.close()
+            cursor.close()
             return df
         except Error as e:
             print(e)
-    
-    #def save_data(self):
-        # Read data into DB
+  
+    def insert_data(self,predictions):
+        #Insert data to database
+        import mysql.connector
+        from mysql.connector import MySQLConnection,Error
+        import pandas as pd
+        try:
+            conn = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=str(self.pw),
+                db = self.db,
+            )
+            cursor = conn.cursor(buffered=True)
+            query = "INSERT INTO" + self.table +"(looks_for_job) VALUES (%s)"
+            cursor.executemany(query,predictions)
+            conn.commit()
+            msg = print(cursor.rowcount, "was inserted.")
+            return msg
+        except Error as e:
+            print(e)
 
-        
-    def query_data(self):
-        #Query data from db
-        self.connect.execute("SELECT * FROM "+ self.table)
+#c = SQL("localhost","root",123456,"hr_analytics","aug_train")
+#c= c.query_data()
+#print(c)
 
-
-c = SQL("localhost","root",123456,"hr_analytics","aug_train")
-c= c.connect()
-print(c)
