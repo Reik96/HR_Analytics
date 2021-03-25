192484769e411535e6b4c7cb593e2dfaa3f34930
Reik Selent <r.selent@web.de>
2021-03-25 23:30:54 +0100

    app feature progress

--- myapp/streamlit_app.py
+++ myapp/streamlit_app.py
@@ -6,8 +6,7 @@ import pickle
 
 model = pickle.load( open( r"C:\Users\rsele\OneDrive\Data Science\Projekt\ML_with_SQL_Tableau\model\log_reg_model.pkl", "rb" ))
 col_transformer = pickle.load( open( r"C:\Users\rsele\OneDrive\Data Science\Projekt\ML_with_SQL_Tableau\src\preprocessing\col_transformer.pkl", "rb" ))
-# import col_transformer.pkl
-# import model.pkl
+
 
 st.write("""
 # Simple HR Prediction App!
@@ -16,33 +15,46 @@ st.write("""
 
 st.sidebar.header('Data Set')
 
-dataset_name = st.sidebar.selectbox(
-    'Select Dataset',
-    ("Training","Test")
+menu_action = st.sidebar.selectbox(
+    'Menu',
+    ("Predict","Visualize","Save")
 )
 
-#if dataset_name == "Test":
+
+
 
 from src.predictions.sql_connection import SQL
 c = SQL("localhost","root",123456,"hr_analytics","aug_test")
 df = c.query_data()
 
-df = cleaning.data_cleaning(df)
 
+
+df_id = df["enrollee_id"]
+df = cleaning.data_cleaning(df)
 
 
 scaled_X = col_transformer.transform(df)
 
 y_pred=model.predict(scaled_X)
+
 y_pred_proba= model.predict_proba(scaled_X)
 
-print(y_pred)
-st.subheader('Class Labels')
-st.write(pd.DataFrame.from_dict(data = {'Label': ["No","Yes"]}))
 
-st.subheader('Data Scientists that are looking for a new job')
 
-st.write(y_pred)
+if menu_action == "Predict":
 
-st.subheader('Prediction Probability')
-st.write(y_pred_proba)
+    st.subheader('Class Labels')
+    st.write(pd.DataFrame.from_dict(data = {'Label': ["No","Yes"]}))
+
+    st.subheader('Data Scientists that are looking for a new job')
+
+    y_pred_proba_0 = pd.Series(y_pred_proba[:,0], name= "No - Probability")
+    y_pred_proba_1 = pd.Series(y_pred_proba[:,1], name= "Yes - Probability")
+    y_pred = pd.Series(y_pred, name= "Prediction")
+
+    st.write(pd.concat([df_id, y_pred,y_pred_proba_0,y_pred_proba_1], axis=1))
+
+
+if menu_action == "Visualize":
+
+    st.bar_chart(y_pred)
