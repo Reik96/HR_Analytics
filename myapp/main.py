c270d1097021ccf91d99fe29698cdb719eeec9cb
Reik Selent <r.selent@web.de>
2021-03-30 15:35:41 +0200

    SQL feature for latest data/ appendin data gsheet

--- myapp/main.py
+++ myapp/main.py
@@ -12,7 +12,7 @@ def main():
     
     data_action = st.sidebar.selectbox(
         'Data Source',
-        ("SQL","CSV")
+        ("All SQL Data","Latest SQL Data","CSV File")
     )
 
     df = data_loading(data_action)
@@ -38,30 +38,41 @@ def main():
         
         st.subheader("Save Predictions")
         csv_downloader(df_conc)
+
         
-        gs_button = st.button("Store Data in Google Spreadsheets")
-        if gs_button == True:
-            try:
-                st.write("Data stored in Google Spreadsheets")
-                data_storing_gs(df_conc)
-            except Exception:
-                pass
-            
-        sql_button = st.button("Store Data in MySQL")
+        sql_button = st.button("Append Data in MySQL")
 
         if sql_button == True:
             st.write("Data stored in MySQL")
             data_storing_sql(df_conc)
+        
+        gs_clear_button = st.button("Replace Data in Google Sheet")
+        if gs_clear_button == True:
+         
+            try:
+                st.write("Replaced Values in Google Sheet")
+                data_storing_gs(df_conc,True)
+            except Exception:
+                pass
+
+        gs_button = st.button("Append Data in Google Sheet")
+        
+        if gs_button == True:
+        
+            try:
+                st.write("Updated Google Sheet")
+                data_storing_gs(df_conc,False)
+            except Exception:
+                pass
+        
+
+    
+            
        
 
     if menu_action == "Visualizations":
-        st.subheader('Tableau Dashboard')
-        dashboard_button = st.button("Update Dashboard")
-
-        if dashboard_button == True:
-            st.write("Yupppp")
-
-
+        st.subheader('Tableau Public Dashboard')
+        st.write("Based on the  Google Sheet, the Dashboard refreshes daily or by requesting an update on Tableau Public ")
         html_temp = """<div class='tableauPlaceholder' id='viz1617009991413' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;HR&#47;HR_Analytics_16170086783530&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='HR_Analytics_16170086783530&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;HR&#47;HR_Analytics_16170086783530&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='de' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1617009991413');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='1227px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
         components.html(html_temp,width = 1960, height = 1080)
     
