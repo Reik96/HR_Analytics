7ce134f0a45ad788898bafa9cc683063e5c92521
Reik Selent <r.selent@web.de>
2021-03-31 14:57:16 +0200

    updated feature importance

--- notebook/feature importance.py
+++ notebook/feature importance.py
@@ -6,27 +6,32 @@ import numpy as np
 import pandas as pd
 from src.modelling.feature_names import get_feature_names
 
-from data_preprocessing import X_train, scaled_X_train, y_train
+from data_preprocessing import scaled_X_train, y_train
 
 #import col transformer and logistic regression
-col_transformer = pickle.load( open( r"C:\Users\rsele\OneDrive\Data Science\Projekt\ML_with_SQL_Tableau\src\preprocessing\col_transformer.pkl", "rb" ))
+col_transformer = pickle.load( open( r"C:\Users\rsele\OneDrive\Data Science\Projekte\HR_Analytics\src\preprocessing\col_transformer.pkl", "rb" ))
 
-model = pickle.load( open( r"C:\Users\rsele\OneDrive\Data Science\Projekt\ML_with_SQL_Tableau\model\log_reg_model.pkl", "rb" ))
+model = pickle.load( open( r"C:\Users\rsele\OneDrive\Data Science\Projekte\HR_Analytics\model\log_reg_model.pkl", "rb" ))
 
 # get feature names from transformed X-Values
 scaled_X_names = get_feature_names(col_transformer)
 
 from sklearn.feature_selection import RFE
 
-#rekursive Feature Elimination
-rfe = RFE(model,10)
+rfe = RFE(model, 20)
 rfe.fit(scaled_X_train, y_train)
-df_feat = pd.DataFrame({"Ranking":rfe.ranking_,
-                        "Feature":scaled_X_names
-                        })
-   
-df_feat = df_feat.sort_values(by="Ranking",ascending=True)
+coefs= np.transpose(rfe.estimator_.coef_)
+feat = [feature for feature, rank in zip(scaled_X_names, rfe.ranking_) if rank==1]
 
-print(df_feat[df_feat["Ranking"]<11])
+print(feat)
 
+df_feat = pd.DataFrame(data=coefs,columns=["coefficients"])
+df_feat["Feature"]=feat
+df_feat = df_feat.sort_values(by="coefficients", ascending=False)
 
+# Diagramm der wichtigsten Features
+plt.barh(df_feat["Feature"],df_feat["coefficients"])
+plt.title("Relevant Features")
+plt.ylabel("Features")
+plt.xlabel("Coefficient")
+plt.show()
